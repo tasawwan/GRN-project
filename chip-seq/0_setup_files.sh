8fcd21ddcddb75485411936e63d939554bfea3ae
tasawwan <tasawwarerahman@gmail.com>
2023-12-15 17:11:00 -0500

    Commit

--- chip-seq/0_setup_files.sh
+++ chip-seq/0_setup_files.sh
@@ -26,6 +26,7 @@ cd ..
 # Copy the only 10 to 12 peaks to the idr_intersect folder and subset for synaptic genes
 cp ../motif_analysis/neurons_10to12/10to12_only.narrowPeak idr_intersect/neurons_10to12_only.narrowPeak
 
+mkdir idr_intersect/synaptic_genes
 module load bedtools/2.26.0
 bedtools window -w 3000 -a ~/data/terahman/synaptic_genes.bed.gz -b idr_intersect/neurons_10to12_only.narrowPeak > idr_intersect/neurons_10to12_only_synaptic_3k.narrowPeak
 bedtools window -w 10000 -a ~/data/terahman/synaptic_genes.bed.gz -b idr_intersect/neurons_10to12_only.narrowPeak > idr_intersect/neurons_10to12_only_synaptic_10k.narrowPeak
